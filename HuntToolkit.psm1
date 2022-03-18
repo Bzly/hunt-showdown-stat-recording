d56db17e7cc62d0a9b21f2eff1fb94b82bbc1b19
Bzly <clampstand@gmail.com>
2022-03-18 15:00:51 +0000

    first draft of proper output

--- HuntToolkit.psm1
+++ HuntToolkit.psm1
@@ -21,12 +21,12 @@ function Get-SteamName {
 # Note does not perform much checking, so you could actually pass an exact value and get a weird result. 
 # Don't do that, and I hope your game isn't called 'installdir'. 
 function Get-ValueFromValveFile {
-    param (
-		[Parameter(Mandatory=$true)]
+    Param (
+        [Parameter(Mandatory=$true)]
         [string]$Path,
         [Parameter(Mandatory=$true)]
-		[string]$Key
-	)
+        [string]$Key
+    )
     $t = Get-Content -Path $Path | Select-String """$Key"""
     return $t | % {$_ -replace ('\s+"' + [regex]::escape($Key) + '"\s+"(.*?)"'),'$1'}
 }
@@ -34,10 +34,10 @@ function Get-ValueFromValveFile {
 # Returns Steam Library dirs as array of directory objects
 # Presumably run like: Get-SteamLibraries -SteamDir (Get-SteamDirectory)
 function Get-SteamLibraries {
-    param (
-		[Parameter(Mandatory=$true)]
-		[string]$SteamDir
-	)
+    Param (
+        [Parameter(Mandatory=$true)]
+        [string]$SteamDir
+    )
     [string[]]$SteamLibs = Get-ValueFromValveFile -Path "$SteamDir\steamapps\libraryfolders.vdf" -Key "path"
     # Return as array of directory objects. Assumes all drives in libraryfolders.vdf are available.
     # Hope you don't have any libraries on removable drives :s
@@ -46,10 +46,10 @@ function Get-SteamLibraries {
 
 # Returns a Steam app install dir as directory object
 function Get-SteamAppDirectory {
-    param (
-		[Parameter(Mandatory=$true)]
-		[string]$AppID
-	)
+    Param (
+        [Parameter(Mandatory=$true)]
+        [string]$AppID
+    )
     $SteamLibs = Get-SteamLibraries -SteamDir (Get-SteamDirectory)
     foreach ($lib in $SteamLibs) {
         $TestPath = "$lib\steamapps\appmanifest_$AppID.acf"
@@ -68,65 +68,125 @@ function Get-HuntAttributes {
     return $x.Attributes.Attr
 }
 
-# Function to convert the useful bits of information from Hunt's attributes.xml to 
-# 	a sensible data structure in JSON
-# Currently this function returns only team and player information. It's a first draft. 
-# Other keys to look at: MissionBagIsQuickPlay, MissionBagIsHunterDead, MissionBagNumAccolades, 
-# 	MissionBagNumEntries, last_given_gift_mission_counter(?)
+# Other keys to look at: MissionBagIsQuickPlay, MissionBagIsHunterDead
 # In MissionBagEntry attr, is "_reward" value the type? 
-	# 0 = Bounty
-	# 2 = XP
-	# 4 = gold
-	# 12 = bloodline xp
-	# 11 = upgrade points
-	# 8 = (christmas?) event points
+    # 0 = Bounty
+    # 2 = XP
+    # 4 = gold
+    # 12 = bloodline xp
+    # 11 = upgrade points
+    # 8 = (christmas?) event points
 function ConvertFrom-HuntAttributes {
+    Param([switch]$Object)
     $Attr = Get-HuntAttributes
+
+    # Get maximum numbers of things that we care about (to filter out old data)
     [int]$NumTeams = ($Attr | Where-Object {$_.name -eq "MissionBagNumTeams"}).value
+    [int]$NumMissEnt = ($Attr | Where-Object {$_.name -eq "MissionBagNumEntries"}).value
+
+    # Get all MissionBagEntry data relating to the last match, and group by entry
+    $MissBag = $Attr | Where-Object {$_.name -like "MissionBagEntry_*_*"} | `
+        Group-Object -Property {$_.name -replace '_[a-z0-9]+$'} | `
+        Where-Object {[int]($_.Name -replace '^.*_') -in 0..($NumMissEnt-1)}
+
+    # Use MissionBag grouped data to return one key (Key) based on the value (Equals) of another key (ConditionKey)
+    [int]$Bounty = $MissBag | HuntGetCondValues -Key "rewardSize" -ConditionKey "reward" -Equals "0" | `
+        Measure-Object -Property value -Sum | Select -ExpandProperty Sum
+    [int]$XP = $MissBag | HuntGetCondValues -Key "rewardSize" -ConditionKey "reward" -Equals "2" | `
+        Measure-Object -Property value -Sum | Select -ExpandProperty Sum
+    [int]$Dollars = $MissBag | HuntGetCondValues -Key "rewardSize" -ConditionKey "reward" -Equals "4" | `
+        Measure-Object -Property value -Sum | Select -ExpandProperty Sum
+    [int]$BloodBonds = $MissBag | HuntGetCondValues -Key "rewardSize" -ConditionKey "reward" -Equals "fillme" | `  # TBD
+        Measure-Object -Property value -Sum | Select -ExpandProperty Sum
+    
+    $XP += 4*$Bounty; $Dollars += $Bounty                                               # adjust for bounty rewarding xp/money
+
+    [int]$Assists = ($MissBag | HuntGetCondValues -Key "amount" -ConditionKey "category" `
+        -Equals "accolade_players_killed_assist").value                                 # can only ever return one entry
 
     # Populate team data
     # Feels like there's probably a smart, recursive way to do this... but it's 4AM
     $TeamData = @()
     foreach ($i in 0..($NumTeams-1)) {
-        $tp = "MissionBagTeam_$($i)_" 													# prefix for current team
-        $td = $Attr | Where-Object {$_.name -like "$tp*"} 								# all Attr elements belonging to team
-        $t = HuntAddKeys -Attributes $td -Dict ([ordered]@{"id" = $i}) -ReplaceStr $tp 	# create individual team
+        $tp = "MissionBagTeam_$($i)_"                                                   # prefix for current team
+        $td = $Attr | Where-Object {$_.name -like "$tp*"}                               # all Attr elements belonging to team
+        $t = HuntAddKeys -Attributes $td -Dict ([ordered]@{"id" = $i}) -ReplaceStr $tp  # create individual team
 
         # Populate player data
         $PlayerData = @()
         foreach ($j in 0..($t.numplayers-1)) {
-            $pp = "MissionBagPlayer_$($i)_$($j)_" 										# prefix for current player
-            $pd = $Attr | Where-Object {$_.name -like "$pp*"}							# all Attr elements belonging to player
+            $pp = "MissionBagPlayer_$($i)_$($j)_"                                       # prefix for current player
+            $pd = $Attr | Where-Object {$_.name -like "$pp*"}                           # all Attr elements belonging to player
             $PlayerData += HuntAddKeys -Attributes $pd `
-				-Dict ([ordered]@{"id" = $j}) -ReplaceStr $pp							# create and add individual player
+                -Dict ([ordered]@{"id" = $j}) -ReplaceStr $pp                           # create and add individual player
         }
-
         $t.players = $PlayerData
         $TeamData += $t
     }
-    return $TeamData | ConvertTo-Json -Depth 100
+
+    # Construct output
+    $Out = [ordered]@{
+        playedas = Get-SteamName
+        isquickplay = [System.Convert]::ToBoolean(($Attr | Where-Object {$_.name -eq "MissionBagIsQuickPlay"}).value)
+        rewards = [ordered]@{
+            bounty = $Bounty
+            xp = $XP
+            money = $Dollars
+            bloodbonds = $BloodBonds
+        }
+        performance = [ordered]@{
+            kills = [int]($TeamData.players.killedbyme | Measure-Object -Sum).Sum
+            deaths = [int]($TeamData.players.killedme | Measure-Object -Sum).Sum
+            assists = $Assists
+            revives = 69
+            survived = [System.Convert]::ToBoolean(($Attr | Where-Object {$_.name -eq "MissionBagIsHunterDead"}).value)
+        }
+        teams = $TeamData
+    }
+
+    if (-not $Object) {return $Out | ConvertTo-Json -Depth 100} else {return $Out}
+}
+
+# Use MissionBag grouped data to return one key (Key) based on the value (Equals) of another key (ConditionKey)
+# Helper function, not exported
+function HuntGetCondValues {
+    Param (
+        [Parameter(Mandatory=$true,ValueFromPipeline)]
+        [array]$GroupedAttr, 
+        [Parameter(Mandatory=$true)]
+        [string]$Key,
+        [Parameter(Mandatory=$true)]
+        [string]$ConditionKey,
+        [Parameter(Mandatory=$true)]
+        $Equals
+    )
+    ($GroupedAttr | Where-Object { `
+            $_.Group | Where-Object {$_.name -like "*_$ConditionKey" `
+            -and $_.value -eq $Equals}
+        }).Group | `
+        Where-Object {$_.name -like "*_$Key"}
 }
 
 # Loops through array of XML attributes, removes the prefix used, and adds them to an ordered dictionary
 # Also performs type conversions for a set list of keys (to int), and for true/false values
 # Helper function, not exported
 function HuntAddKeys {
-    param (
-		[Parameter(Mandatory=$true)]
+    Param (
+        [Parameter(Mandatory=$true)]
         [array]$Attributes, #td
         [Parameter(Mandatory=$false)]
-		[System.Collections.Specialized.OrderedDictionary]$Dict = [ordered]@{},
+        [System.Collections.Specialized.OrderedDictionary]$Dict = [ordered]@{},
         [Parameter(Mandatory=$true)]
-		[string]$ReplaceStr
-	)
+        [string]$ReplaceStr
+    )
     $NumericKeys = @("mmr", "numplayers", "handicap", "profileid", "killedbyme", "killedme")
     $Attributes | % {
         $key = $_.name -replace "$ReplaceStr"
         if ($key -in $NumericKeys) {
-            $Dict[$key] = [Int64]$_.value # profileid > [int]::MaxValue, so use long
+            $Dict[$key] = [Int64]$_.value # "profileid" > [int]::MaxValue
         } elseif ($_.value -in @("true", "false")) {
-			$Dict[$key] = [System.Convert]::ToBoolean($_.value)
-		} else {
+            $Dict[$key] = [System.Convert]::ToBoolean($_.value)
+        } else {
             $Dict[$key] = $_.value
         }
     }
